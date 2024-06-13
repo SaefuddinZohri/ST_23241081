1ab187b1f0dfe14c6e3f9195e59124ae1ff8a398
SaefuddinZohri <148041253+SaefuddinZohri@users.noreply.github.com>
2024-06-13 14:57:31 +0800

    Add files via upload

---
+++ meet9.py
@@ -0,0 +1,12 @@
+#Belajar mulit percabangan
+
+nama = input("masukan nama")
+if nama=="zohri": # kondisi 1
+    print("zohri ganteng") # aksi true 1
+elif nama=="nayla": # kondisi 2
+    print("nayla cantik") # aksi true 2  
+elif nama=="putra" : # kondisi 3
+    print("putra gk ganteng") # aksi true 3
+else:
+    print("anda bukan siapa-siapa") # false   
+print("program selesai") # aksi end             
